008062fd1ab85575a4fa296a709091593d191bf5
LuisCruz-1 <luiscruzm.ec@gmail.com>
2025-03-09 23:10:16 -0500

    fix logo and font

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Menu.module.css';
+import logo from '../../assets/iconANAVI.png'
 
 const Menu = () => {
   return (
     <header className={styles.menuContainer}>
       <div className={styles.logo}>
-        <img src="/src/assets/iconANAVI.png" alt="" />
+        <img src={logo} alt="" />
         Colegio ANAVI - Vacacional</div>
     </header>
   );
